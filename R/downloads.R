626e3fcfae68e6f5d69040963753db6e76453b4f
Clint Chelak <CChelak@users.noreply.github.com>
2017-05-29 19:25:45 -0600

    The first few
    I put in just the basic first few today. Though, admittedly, I don't know which data fits the model I previously saw of "Location," "Species," and "count"

--- R/downloads.R
+++ R/downloads.R
@@ -5,7 +5,7 @@
 #' @importFrom fulltext ft_get_si
 
 .get.addresses <- function (...){
-# Here is my first thought to conqure this. It saves a bunch of function calls, at the cost of individual customizability.
+# Here is my first thought to conquer this. It saves a bunch of function calls, at the cost of individual customizability.
 # Places all addresses in a single list (or should it be a vector?) Will be accessed and processed by another funciton
 # Each quote could have corresponding data for alterations that would need to be made to the csv or table
     
@@ -23,14 +23,30 @@
 # Option 2 is the manual version that looks very similar to downloads.R in natb, taking one publication at a time
 # and creating a function call for it. I'll do a few here and for you to see if I'm doing it right.
 
+# the location data is incredibly hard to follow. Couldn't find correlation to the species count. Just took species count.
 .adler.2007 <- function(...){
-    data <- read.csv(ft_get_si()) #No progress on this.
+    data <- read.csv("http://esapubs.org/archive/ecol/E088/161/species_list.csv")[c(1,3,2)]
+    
+    return(data)
+}
 
+.anderson.2011 <- function(...){
+    data <- read.csv("https://ndownloader.figshare.com/files/5619462")
+    data <- data[order(data$species), c(3, 1, 2, 4)
+    return (data)
+}
+
+.chu.2013 <- function(...){
+    #contains plant species and a count, but no location
+    species.count <- read.csv("https://ndownloader.figshare.com/files/5626719")
+    
+    #contains location and cattle count but no corresponding species
+    cattle.count <- read.csv("https://ndownloader.figshare.com/files/5626713")
 }
 
 
 
-.helmus.2013 <- function(...){
+.hnselmus.2013 <- function(...){
     library(pez) # This isn't how we declare packages in 'real'
                  # packages for the time being this is sufficient
     data(laja)
